f2a353d786eebc30bcc5c091f9c66f1866354591
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-19 00:55:23 -0700

    commit tree 257dfcd78077c7fb61dc1ba8b953dd121e86f8bb

--- index.js
+++ index.js
@@ -4,10 +4,65 @@
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
-
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+
+
+ function createEmployeeRecord(array){
+    return new Object({
+        firstName : array[0],
+        familyName : array[1],
+        title : array[2],
+        payPerHour : array[3],
+        timeInEvents :[],
+        timeOutEvents :[]
+})
+
+}
+
+let createEmployeeRecords = function(newArray){
+    return newArray.map(function(array){
+        return createEmployeeRecord(array)
+    })
+}
+let createTimeInEvent= function(dates){
+ let [date,hour] =dates.split('')
+
+ this.timeInEvents.push({
+    type: "TimeIn",
+    date,
+    hour :parseInt(hour,10),
+ })
+ return this
+}
+
+let createTimeOutEvent= function(datesOut){
+    let [date,hour] =datesOut.split('')
+   
+    this.timeOutEvents.push({
+       type: "TimeOut",
+       date,
+       hour :parseInt(hour,10),
+    })
+    return this
+   }
+
+
+
+
+function hoursWorkedOnDate (recordObject, param1){
+    let hours = (recordObject.timeOutEvents[0].hour -recordObject.timeInEvents[0].hour)/100
+    return hours
+}
+
+
+
+function wagesEarnedOnDate(recordObject,param1){
+    let wages = hoursWorkedOnDate(recordObject, param1)*recordObject.payPerHour
+    return wages
+
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +76,16 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+
+
+function calculatePayroll(employees){
+ let total = 0
+ for (let employee of employees){
+    console.log(employee)
+    total += allWagesFor(employee)
+ }
+ return total 
+}
+console.log (calculatePayroll(employees))
